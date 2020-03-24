baa0b68ad6aad51085918ab8f0aadc2c258d69b0
MrSamuel137 <samuelcoudounarakis41@gmail.com>
2020-03-23 21:25:40 -0300

    feat(kotlin): adiciona base do projeto

---
+++ src/api/kotlin/n-livro-preto/src/main/kotlin/an0n/nlivropreto/NLivroPretoApplication.kt
@@ -0,0 +1,11 @@
+package an0n.nlivropreto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NLivroPretoApplication
+
+fun main(args: Array<String>) {
+	runApplication<NLivroPretoApplication>(*args)
+}
---
+++ src/api/kotlin/n-livro-preto/src/test/kotlin/an0n/nlivropreto/NLivroPretoApplicationTests.kt
@@ -0,0 +1,13 @@
+package an0n.nlivropreto
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NLivroPretoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
